78c7a8ce1998cc3782f0bd13bb1fa4c81239f589
Gowtham <gowtham2001k@gmail.com>
2022-12-24 16:24:14 +0530

    level 3 -"added route for products and homeScreen "

--- amazonClone/frontend/src/App.js
+++ amazonClone/frontend/src/App.js
@@ -1,34 +1,22 @@
-import data from './data';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './Screen/HomeScreen';
+import ProductScreen from './Screen/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazon</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/products/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/products/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to Cart</button>
-                {/* <button>Buy now</button> */}
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazon</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/products/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
