d1a61c90dd25d0ddf5892e40387d40163d61f9fb
Gowtham <gowtham2001k@gmail.com>
2022-12-26 13:13:28 +0530

    level 17 - signIn API was created and tested using postman

--- amazonClone/Backend/server.js
+++ amazonClone/Backend/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRoutes from './routes/productRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -19,10 +20,18 @@ mongoose
   });
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRouter);
 
 const port = process.env.PORT || 5000;
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 app.listen(port, () => {
   console.log(`server running at port ${port} successfully`);
