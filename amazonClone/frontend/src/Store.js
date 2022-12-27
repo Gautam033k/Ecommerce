55db77a07af90df3e043db0dc099e6ed568dd2fa
Gowtham <gowtham2001k@gmail.com>
2022-12-27 16:34:00 +0530

    level 20 - completed payment Screen

--- amazonClone/frontend/src/Store.js
+++ amazonClone/frontend/src/Store.js
@@ -12,6 +12,10 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : [],
+
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -47,7 +51,7 @@ function reducer(state, action) {
       return {
         ...state,
         userInfo: null,
-        cart: { cartItems: [], shippingAddress: {} },
+        cart: { cartItems: [], shippingAddress: {}, paymentMethod: '' },
       };
     case 'SAVE_SHIPPING_ADDRESS':
       return {
@@ -55,6 +59,14 @@ function reducer(state, action) {
         cart: {
           ...state.cart,
           shippingAddress: action.payload,
+        },
+      };
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
     default:
